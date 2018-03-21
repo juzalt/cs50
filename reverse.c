ae00f6b0273bad9cc5bd37df6ad9204e3f962521
juzalt <juzalt@users.noreply.github.com>
2018-03-20 22:45:06 -0300

    Add files via upload

---
+++ reverse.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+#include <cs50.h>
+/*This program will reverse any string.*/
+char n;
+char get_string();
+
+int main(){
+  char n = get_string("What do you wanna see in the mirror?");
+  return 0;
+}
